e0b8f009b33bb2eaa8d47e7b1f139d3956e80d6c
thiago <thg.humble@gmail.com>
2024-10-01 08:24:59 -0300

    comunicação com o formulário

--- src/app/pages/company-registration/company-registration.component.ts
+++ src/app/pages/company-registration/company-registration.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-company-registration',
   standalone: true,
   imports: [
-    ContainerComponent
+    ContainerComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './company-registration.component.html',
   styleUrl: './company-registration.component.css'
 })
 export class CompanyRegistrationComponent {
+  formContact!: FormGroup;
 
+  constructor() {
+    this.formContact = new FormGroup({
+      companyType: new FormControl('mkt'),
+      companyName: new FormControl('thumble mkt digital'),
+      cpnj: new FormControl('444444444444'),
+      cep: new FormControl('00000000'),
+      address: new FormControl('rua Bruno CF'),
+      neighborhood: new FormControl('vila prudente'),
+      state: new FormControl('SP'),
+      city: new FormControl('SP'),
+      complement: new FormControl('61A'),
+      mobile: new FormControl('9999999-9999'),
+      adm: new FormControl('Thumble'),
+      cpf: new FormControl('999999999-99'),
+      email: new FormControl('thg.humble@gmail.com')
+    })
+  }
 }
--- src/app/pages/createaccount/createaccount.component.ts
+++ src/app/pages/createaccount/createaccount.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-createaccount',
   standalone: true,
   imports: [
-    ContainerComponent
+    ContainerComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './createaccount.component.html',
   styleUrl: './createaccount.component.css'
 })
 export class CreateaccountComponent {
+  accountForm!: FormGroup;
 
+  constructor() {
+    this.accountForm = new FormGroup({
+      name: new FormControl('Thiago'),
+      email: new FormControl('thg.humble@gmail.com'),
+      psw: new FormControl('Thiago'),
+      pswAgain: new FormControl('Thiago')
+    })
+  }
 }
