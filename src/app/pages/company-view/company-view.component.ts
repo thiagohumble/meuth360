12a2f9bb710ab724c7ec15f45a2ef2a99fdaf464
thiago <thg.humble@gmail.com>
2024-10-01 22:48:52 -0300

    mostra empresas cadastradas

--- src/app/pages/company-view/company-view.component.ts
+++ src/app/pages/company-view/company-view.component.ts
@@ -1,36 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { ContactService } from '../../services/contact.service';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 interface Company {
-  companyName: "string"
-  cpnj: number
-  cep: number
-  address: string
-  neighborhood: string
-  mobile: number
-  adm: string
-  cpf: number
-  email: string
+  companyName: string;
+  cpnj: number;
+  cep: number;
+  address: string;
+  neighborhood: string;
+  mobile: number;
+  adm: string;
+  cpf: number;
+  email: string;
 }
 
 @Component({
   selector: 'app-company-view',
   standalone: true,
   imports: [
-    ContainerComponent
+    ContainerComponent,
+    FormsModule // Include FormsModule here
   ],
   templateUrl: './company-view.component.html',
-  styleUrl: './company-view.component.css'
+  styleUrls: ['./company-view.component.css']
 })
 export class CompanyViewComponent implements OnInit {
-
-  companyAll: Company[] = []
+  company: Company = {
+    companyName: '',
+    cpnj: 0,
+    cep: 0,
+    address: '',
+    neighborhood: '',
+    mobile: 0,
+    adm: '',
+    cpf: 0,
+    email: '',
+  };
   
   constructor(private companyService: ContactService) {}
 
-  //boa praticas hook siclo de vida
   ngOnInit() {
-   this.companyAll = this.companyService.getCompany();
+    const companies = this.companyService.getCompany();
+    if (companies.length > 0) {
+      this.company = companies[0];
+    }
   }
 }
