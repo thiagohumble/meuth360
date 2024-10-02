2c4855966ebfd871ee0d9726b1e12039fafacd11
thiago <thg.humble@gmail.com>
2024-10-01 22:21:09 -0300

    usando boas praticas OnInit ciclo de vida

--- src/app/pages/company-view/company-view.component.ts
+++ src/app/pages/company-view/company-view.component.ts
@@ -1,5 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
+import { ContactService } from '../../services/contact.service';
+
+interface Company {
+  companyName: "string"
+  cpnj: number
+  cep: number
+  address: string
+  neighborhood: string
+  mobile: number
+  adm: string
+  cpf: number
+  email: string
+}
 
 @Component({
   selector: 'app-company-view',
@@ -10,7 +23,14 @@ import { ContainerComponent } from '../../componentes/container/container.compon
   templateUrl: './company-view.component.html',
   styleUrl: './company-view.component.css'
 })
-export class CompanyViewComponent {
-  
+export class CompanyViewComponent implements OnInit {
 
+  companyAll: Company[] = []
+  
+  constructor(private companyService: ContactService) {}
+
+  //boa praticas hook siclo de vida
+  ngOnInit() {
+   this.companyAll = this.companyService.getCompany();
+  }
 }
